0d790713494b0d90aa6bcf0ef03bb724347d4223
neledov <neledov@gmail.com>
2025-12-18 22:28:23 +0100

    release: v0.1.6_iddqd_hotfix - the pig confesses
    suspects froze. we fixed it anyway.
    busy guards added. memory released. pins unswapped.
    the documentation knew. the settings menu forgot.
    
    long live the pig.

--- src/build_info.h
+++ src/build_info.h
@@ -1,4 +1,4 @@
 // Auto-generated build info
 #pragma once
-#define BUILD_TIME "2025-12-18T14:30:25.599924"
-#define BUILD_VERSION "0.1.6_iddqd"
+#define BUILD_TIME "2025-12-18T22:09:28.327211"
+#define BUILD_VERSION "0.1.6_iddqd_hotfix"
