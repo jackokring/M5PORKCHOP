c4f8ad7940957446925a160191e0d064687cac32
neledov <neledov@gmail.com>
2025-12-22 15:05:09 +0100

    release: v0.1.7_forgot_my_meds_hotfix01
    ??? lock mystery solved. buffer overflow prevented.
    pig stops staring at ghosts. strlen learns humility.
    
    i suck. my vacuum not. it broke. no sucky.
    jah bless.

--- src/build_info.h
+++ src/build_info.h
@@ -1,5 +1,5 @@
 // Auto-generated build info
 #pragma once
-#define BUILD_TIME "2025-12-22T13:04:37.181954"
-#define BUILD_VERSION "0.1.7_forgot_my_meds"
-#define BUILD_COMMIT "b707721"
+#define BUILD_TIME "2025-12-22T14:49:54.446551"
+#define BUILD_VERSION "0.1.7_forgot_my_meds_hotfix01"
+#define BUILD_COMMIT "0eb403b"
