24307ac2f33e75ac2188ca8c2fc47836ebe30fc7
jackokring <jackokring@gmail.com>
2026-02-10 13:28:25 +0000

    Tuesday 2026-02-10 13:28:25

--- src/build_info.h
+++ src/build_info.h
@@ -1,5 +1,5 @@
 // Auto-generated build info
 #pragma once
-#define BUILD_TIME "2026-01-30T21:11:19.800835"
-#define BUILD_VERSION "0.1.8a_heapkampf"
-#define BUILD_COMMIT "6337c46"
+#define BUILD_TIME "2026-02-10T13:27:53.765545"
+#define BUILD_VERSION "0.1.8b-PSTH"
+#define BUILD_COMMIT "add82d6"
